08aebfdb198c80a37ca54443ea830f25a25909c8
TikZSZ <repackergame@gmail.com>
2022-11-08 01:38:38 +0530

    added support for light mode and dark mode, fixed intro image, renamed jhatcut to jhattcut, set up cname to jhattcut.com

--- index.ts
+++
@@ -1,9 +0,0 @@
-const menuBar = document.getElementById("menu-bar");
-const navLinks = document.getElementById("links");
-
-menuBar?.addEventListener("click", () => {
-  // const mq = window.matchMedia( "max-width: 700px" );
-  // if(document.media)
-  (navLinks as HTMLElement).style.display =
-    (navLinks as HTMLElement).style.display === "none" ? "flex" : "none";
-});
